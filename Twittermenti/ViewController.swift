08d6d29fe2dbce7b5401ac69b007121ac6f71308
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-03-01 23:14:46 -0800

    [Add] initial project

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Hanna Putiprawan on 03/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
