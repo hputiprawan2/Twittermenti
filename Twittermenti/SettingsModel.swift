adf853d2d5878e5d996fc547f225c8b7fa272d23
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-03-02 23:51:37 -0800

    [Add] Twitter API

---
+++ Twittermenti/SettingsModel.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Twittermenti
+//
+//  Created by Hanna Putiprawan on 3/2/21.
+//
+
+import Foundation
+
+//struct SettingsModel: Codable {
+//    let API_Key: String
+//    let API_Key_Secret: String
+//    
+//    func loadAPIKey() {
+//        if let path = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
+//            let xml  = FileManager.default.contents(atPath: path),
+//            let settings = try? PropertyListDecoder().decode(SettingsModel.self, from: xml) {
+//            print(settings.API_Key)
+//            print(settings.API_Key_Secret)
+//        }
+//    }
+//}
--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -6,20 +6,27 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    let swifter = Swifter(consumerKey: "91TSj3HgnUtY8CHKSsvPlasrJ", consumerSecret: "M0rxJG8ohNR8z0Ius4BJml5GttNRXNUdniatfZkiRGrIJWGRd5")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (result, metadata) in
+            print(result)
+        } failure: { (error) in
+            print("There was an error with the Twitter API request, \(error)")
+        }
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-    
     
     }
     
